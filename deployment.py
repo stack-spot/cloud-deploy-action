d516c19d32e39d66c9b55db9da060a1508f0058f
evellyn <evellyn.costa@zup.com.br>
2024-12-27 15:57:30 -0300

    feat(app): change req

--- deployment.py
+++ deployment.py
@@ -42,9 +42,11 @@ def authentication(CLIENT_REALM, CLIENT_ID, CLIENT_KEY):
 def deployment(application_name, runtime_id, deploy_headers, json_data):
     print(f'⚙️ Deploying application "{application_name}" in runtime: "{runtime_id}".')
     deploy_url = "https://cloud-cloud-runtime-api.prd.stackspot.com/v1/deployments"
-    # Ensure Content-Type is application/json
-    deploy_headers["Content-Type"] = "application/json; charset=utf-8"
-    response = requests.post(url=deploy_url, headers=deploy_headers, data=json_data.encode('utf-8'))
+    headers = {
+        "Authorization": deploy_headers["Authorization"],
+        "Content-Type": "application/json"
+    }
+    response = requests.post(url=deploy_url, headers=headers, data=json_data)
     if response.status_code == 200:
         deployment_id = response.json().get("deploymentId")
         print(f"✅ DEPLOYMENT successfully started with ID: {deployment_id}")
